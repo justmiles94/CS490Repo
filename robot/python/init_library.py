47ef5f1324941a24e254a93d3b1cc499e1849fd3
jacob <janovos@siue.edu>
2018-12-04 09:45:08 -0600

    udates

---
+++ robot/python/init_library.py
@@ -0,0 +1,67 @@
+import serial
+import math
+import time
+import numpy as np
+
+#sets up serial connection to arduino atMega
+ser = serial.Serial('/dev/ttyACM0',115200, timeout=.2);
+
+#wait for serial connection to be established
+time.sleep(1)
+
+#clear buffers just incase garbage inside
+ser.reset_input_buffer()
+ser.reset_output_buffer()
+
+pid = 0;
+
+
+# This functions sends  pwm signals to the motor and reverses the direction if given negative
+# Example motor(255,0,0) would turn motor 0 on all the away and 1,2 off
+# motor(125,-200,-100) motor 0 would have a half duty cycle, motor 1 would move backwards at a pwm of 200 etc...
+def motors(m1,m2,m3):
+	motorValues = [m1,m2,m3]
+	for x in range(3):
+		ser.write(("m %d %d %d\r" % (x, abs(motorValues[x]), int(motorValues[x]>=0))).encode())
+
+
+
+#read encoder value from motor number given
+def encoder(encoderNum):
+	ser.reset_input_buffer()
+	ser.write(("e %d \r" % (encoderNum)).encode())
+	encoderValue = (ser.readline().decode("ascii"))
+	return encoderValue.rstrip()
+
+def ultrasound(ultraSoundNum):
+	#ser.reset_input_buffer()
+	ser.write(("u %d \r" % (ultraSoundNum)).encode())
+	ultraSoundValue = (ser.readline().decode("ascii"))
+	return ultraSoundValue.rstrip()
+
+def infrared(infraredNum):
+	ser.reset_input_buffer()
+	ser.write(("i %d \r" % (infraredNum)).encode())
+	infraredValue = (ser.readline().decode("ascii"))
+	return infraredValue.rstrip()
+
+
+def rpm(rpmNum):
+	ser.reset_input_buffer()
+	ser.write(("r %f \r" % (rpmNum)).encode())
+	rpmValue = (ser.readline().decode("ascii"))
+	return rpmValue.rstrip()
+
+def enablePID(pidValue):
+	pid = pidValue
+	ser.write(("p %d \r" % (pid)).encode())
+
+
+
+
+
+
+# assume PID is always off
+
+enablePID(0)
+
---
+++ robot/python/test.py
@@ -0,0 +1,45 @@
+import init_library as robot  #Library tha handles all the serial commands to arduino AtMega
+import time
+
+
+while True:
+	control = input("Enter mode: Motor Command = m, ultrasonic = u, infrared = i, encoder = e >")
+	
+
+	if control == 'm':
+		motorNum = int(input("Enter motor number (0-2): "))
+		motorPWM = int(input("Enter PWM value (0-255): "))
+		motorDir = (input("Enter motor dir (c or ccw): "))
+		if(motorDir == 'ccw'):
+			motorPWM= motorPWM*-1
+		if(motorNum == 0):
+			robot.motors(motorPWM,0,0)
+		if(motorNum == 1):
+			robot.motors(0,motorPWM,0)
+		if(motorNum == 2):
+			robot.motors(0,0,motorPWM)
+		time.sleep(2)
+		rpm = motorNum
+		nu = robot.rpm(rpm)
+		print(nu)
+			
+		
+		
+
+	elif control == 'u':
+		ultraNum = int(input("Enter ultrasonic sensor number (0-5) >"))
+		while True:
+			print("Distance from sensor (cm): "+robot.ultrasound(ultraNum))
+ 
+		
+	
+
+	elif control == 'i':
+		infraredNum = int(input("Enter infrared sensor number (0-3) >"))
+		while True:
+			print("Distance from sensor (cm): "+robot.infrared(infraredNum))
+	
+	elif control == 'e':
+		encoderNum = int(input("Enter encoder motor number (0-2) >"))
+		while True:
+			print("Encoder ticks: "+robot.encoder(encoderNum))
